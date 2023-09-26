509a0116e6ed6da21e5c4dc863d2186c5324d24d
Javier Martinez <jmartinezm22@fpcoruna.afundacion.org>
2023-09-26 09:28:19 +0200

    Ejercicio 2 python completado

--- sprint0python/adivinanza.py
+++ sprint0python/adivinanza.py
@@ -1,13 +1,34 @@
 list_adivin = [
-    "Adivina adivinanza, ¿qué tiene el rey en la panza?",
-    "a. El ombligo.",
-    "b. La corona.",
-    "c. Un donut.",
-    1
+    [
+        "Adivina adivinanza, ¿qué tiene el rey en la panza?",
+        "a. El ombligo.",
+        "b. La corona.",
+        "c. Un donut.",
+        1
+    ],
+    [
+        "Oro parece, plata no es, quien no lo adivine, muy listo no es. ¿Qué es?",
+        "a. Plata.",
+        "b. Un limón.",
+        "c. Un plátano.",
+        3
+    ],
+    [
+        "En este banco hay sentados un padre y un hijo. El padre se llama Juan, y el hijo ya te lo he dicho.\n¿Cómo se llama el hijo?",
+        "a. Juan.",
+        "b. Esteban.",
+        "c. Guamedo.",
+        2
+    ]
 ]
-print(list_adivin[0]+"\n\t"+list_adivin[1]+"\n\t"+list_adivin[2]+"\n\t"+list_adivin[3])
-answer = input("\nEscriba a/b/c: ")
-if answer == list_adivin[list_adivin[4]][0]:
-    print("\tCORRECTO!")
-else:
-    print("INCORRECTO!")
+score = 0
+for i in range(3):
+    print(list_adivin[i][0]+"\n\t"+list_adivin[i][1]+"\n\t"+list_adivin[i][2]+"\n\t"+list_adivin[i][3])
+    answer = input("\nEscriba a/b/c: ")
+    if answer == list_adivin[i][list_adivin[i][4]][0]:
+        print("\tCORRECTO!")
+        score = score + 10
+    else:
+        print("INCORRECTO!")
+        score = score - 5
+print("Puntuación: "+str(score))
