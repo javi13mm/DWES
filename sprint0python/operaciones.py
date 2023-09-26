9b1f5200117c14af1d55990abdf1d85edaa8e8f7
Javier Martinez <jmartinezm22@fpcoruna.afundacion.org>
2023-09-26 09:58:46 +0200

    Ejercicio 3 python completado

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,11 @@
+def suma(a,b):
+    return a+b
+def rest(a,b):
+    return a-b
+def mult(a,b):
+    return a*b
+def divi(a,b):
+    if b!=0 :
+        return a/b
+    else:
+        return "ERROR: (DIV /0)."
