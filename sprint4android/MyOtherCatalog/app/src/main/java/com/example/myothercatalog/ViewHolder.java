9d20fb74afcebc2c06c895ba9650346f1414f3b3
Javier Martinez <jmartinezm22@fpcoruna.afundacion.org>
2023-11-27 12:18:30 +0100

    Ejercicio 2 sprint4android completado

--- sprint4android/MyOtherCatalog/app/src/main/java/com/example/myothercatalog/ViewHolder.java
+++ sprint4android/MyOtherCatalog/app/src/main/java/com/example/myothercatalog/ViewHolder.java
@@ -1,7 +1,9 @@
 package com.example.myothercatalog;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,12 +15,14 @@ import com.bumptech.glide.Glide;
 public class ViewHolder extends RecyclerView.ViewHolder {
     private final TextView name;
     private final ImageView image;
+    private Button button;
 
     //ViewHolder que adquiere los elementos de view_holder.xml y actúa como celda
     public ViewHolder(@NonNull View itemView) {
         super(itemView);
         name = (TextView) itemView.findViewById(R.id.tenista_text_view);
         image = (ImageView) itemView.findViewById(R.id.tenista_image_view);
+        button = (Button) itemView.findViewById(R.id.tenista_button);
     }
 
     //Función que asigna datos a las celdas ViewHolder
@@ -32,5 +36,13 @@ public class ViewHolder extends RecyclerView.ViewHolder {
                 .error(R.drawable.ic_launcher_foreground)
                 .circleCrop()
                 .into(image);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Al clickar el botón, se lanza la DetailActivity
+                Intent intentDetailActivity = new Intent(view.getContext(), DetailActivity.class);
+                view.getContext().startActivity(intentDetailActivity);
+            }
+        });
     }
 }
